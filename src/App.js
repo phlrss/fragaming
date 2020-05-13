16bac5a5fab2177c9a52582553ae12a3122dcea4
Philip Russo <philip.russo@hotmail.com>
2020-05-13 13:13:28 -0400

    added google and apple store badge buttons

--- src/App.js
+++ src/App.js
@@ -8,11 +8,18 @@ function App() {
   return (
     <div className="App">
       <FraHeader />
-      <div className='fra-content'>
-        <FraFeaturedGame gameTitleLine1='Hammy' gameTitleLine2='Hop' />
-        <img className='fra-featured-screenshot' src={HammyScreenshot} alt='featured' />
+      <div className='fra-featured'>
+        <div className='left-half half'>
+          <FraFeaturedGame gameTitleLine1='Hammy' gameTitleLine2='Hop' />
+        </div>
+        <div className='right-half half'>
+          <img className='fra-featured-screenshot' src={HammyScreenshot} alt='featured' />
+        </div>
       </div>
-      <div className='fra-footer'></div>
+      <div className='fra-footer'>
+        <button className='fra-store-badge google'></button>
+        <button className='fra-store-badge apple'></button>
+      </div>
     </div>
   );
 }
