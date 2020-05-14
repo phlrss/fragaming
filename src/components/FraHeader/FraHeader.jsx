02846070915e378e2e843ace79ed6d3995cf96d0
Philip Russo <philip.russo@hotmail.com>
2020-05-13 21:15:11 -0400

    changed logo into image from text

--- src/components/FraHeader/FraHeader.jsx
+++ src/components/FraHeader/FraHeader.jsx
@@ -5,7 +5,7 @@ import FraButton from '../FraButton';
 const FraHeader = () => {
   return (
     <div className='header-container'>
-      <FraButton style='main' text='Fra Gaming' />
+      <FraButton style='main' text='' />
       <FraButton text='Games' />
       <FraButton text='About Us' />
       <FraButton text='Link 3?' />
