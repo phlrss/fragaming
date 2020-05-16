ab573165ad6018fd830bc7bd0f48a92c13bf24ff
Philip Russo <philip.russo@hotmail.com>
2020-05-15 21:24:09 -0400

    work on about us page

--- src/pages/AboutUsPage/AboutUsPage.jsx
+++ src/pages/AboutUsPage/AboutUsPage.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import './AboutUsPage.scss';
+import HammyScreenshot from '../../images/hammy-screenshot.jpeg';
+import FraLogo from '../../images/fra-gaming-logo.png';
 
 const AboutUsPage = () => {
   return (
     <div className="about-us-wrapper">
       <div className="left-half half">
-        left
+        <img src={HammyScreenshot} alt="pic" />
       </div>
       <div className="right-half half">
-        right
+        <img src={FraLogo} alt="logo" />
+        <span>Lorem ipsum bla test random text filler bla bla hello world this is a paragraph</span>
       </div>
     </div>
   )
